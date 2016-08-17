6c6fa81f1872354d7e600b5da14d22282e4c80ae
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-08-16 21:16:43 -0400

    Moved eval result into object

--- src/commands/general/eval.js
+++ src/commands/general/eval.js
@@ -10,8 +10,6 @@ import FriendlyError from '../../errors/friendly';
 import CommandFormatError from '../../errors/command-format';
 /* eslint-enable no-unused-vars */
 
-let lastResult;
-
 export default class EvalCommand extends Command {
 	constructor(bot) {
 		super(bot);
@@ -21,6 +19,8 @@ export default class EvalCommand extends Command {
 		this.description = 'Evaluates input as JavaScript.';
 		this.usage = 'eval <script>';
 		this.details = 'Only the bot owner may use this command.';
+
+		this.lastResult = null;
 	}
 
 	isRunnable(message) {
@@ -31,8 +31,8 @@ export default class EvalCommand extends Command {
 		if(!args[0]) throw new CommandFormatError(this, message.server);
 		const msg = message; // eslint-disable-line no-unused-vars
 		try {
-			lastResult = eval(args[0]);
-			return `Result: \`${util.inspect(lastResult, { depth: 0 })}\``;
+			this.lastResult = eval(args[0]);
+			return `Result: \`${util.inspect(this.lastResult, { depth: 0 })}\``;
 		} catch(err) {
 			return `Error while evaluating: ${err}`;
 		}
