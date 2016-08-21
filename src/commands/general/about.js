6079baa1e2e548d4ccac6dd520fd7409f58b82fb
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-08-20 23:01:20 -0400

    Added GRAF version to about

--- src/commands/general/about.js
+++ src/commands/general/about.js
@@ -1,6 +1,7 @@
 'use babel';
 'use strict';
 
+import { version } from '../..';
 import { stripIndents } from 'common-tags';
 import Command from '../command';
 
@@ -29,8 +30,8 @@ export default class AboutCommand extends Command {
 				This bot ${owner ? `is owned by ${owner.name}#${owner.discriminator}, and ` : ''}is serving ${users} ${usersLabel} across ${servers} ${serversLabel}.
 				It has been running without interruption for ${days > 0 ? `${daysStr} ` : ''}${hours > 0 ? `${hoursStr} ` : ''}${minutesStr}.
 				${config.invite ? `For bot feedback/help, use this invite: ${config.invite}` : ''}
-				
-				Based on Discord GRAF: https://github.com/Gawdl3y/discord-graf
+
+				Based on Discord GRAF v${version}: https://github.com/Gawdl3y/discord-graf
 			`,
 			reply: 'Sent a DM to you with information.'
 		};
