46c1d582aa0297e822bda359ea10cd85fd733449
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-08-22 18:00:24 -0400

    Added ability to load config from yargs without adding options

--- src/bot/config.js
+++ src/bot/config.js
@@ -46,160 +46,164 @@ export default class BotConfig {
 	/**
 	 * Loads configuration from yargs, adding a bunch of default bot options
 	 * @param {Yargs} yargs - The yargs instance to use
+	 * @param {boolean} [addOptions=true] - Whether or not to add options for GRAF's config
 	 * @return {Yargs} The yargs instance
 	 */
-	yargs(yargs) {
-		this.values = yargs
-			// Authentication
-			.option('token', {
-				type: 'string',
-				alias: 't',
-				describe: 'API token for the bot account',
-				group: 'Authentication:'
-			})
-			.option('email', {
-				type: 'string',
-				alias: 'e',
-				describe: 'Email of the Discord account for the bot to use',
-				group: 'Authentication:'
-			})
-			.option('password', {
-				type: 'string',
-				alias: 'p',
-				describe: 'Password of the Discord account for the bot to use',
-				group: 'Authentication:'
-			})
-			.implies({ email: 'password', password: 'email' })
+	yargs(yargs, addOptions = true) {
+		if(addOptions) {
+			yargs
+				// Authentication
+				.option('token', {
+					type: 'string',
+					alias: 't',
+					describe: 'API token for the bot account',
+					group: 'Authentication:'
+				})
+				.option('email', {
+					type: 'string',
+					alias: 'e',
+					describe: 'Email of the Discord account for the bot to use',
+					group: 'Authentication:'
+				})
+				.option('password', {
+					type: 'string',
+					alias: 'p',
+					describe: 'Password of the Discord account for the bot to use',
+					group: 'Authentication:'
+				})
+				.implies({ email: 'password', password: 'email' })
 
-			// General
-			.option('owner', {
-				type: 'string',
-				alias: 'o',
-				describe: 'Discord user ID of the bot owner',
-				group: 'General:'
-			})
-			.option('invite', {
-				type: 'string',
-				alias: 'i',
-				describe: 'Discord instant invite to a server to contact the owner',
-				group: 'General:'
-			})
-			.option('playing-game', {
-				type: 'string',
-				default: this.defaults.playingGame,
-				alias: 'g',
-				describe: 'Text to show in the "Playing..." status',
-				group: 'General:'
-			})
-			.option('pagination-items', {
-				type: 'number',
-				default: this.defaults.paginationItems,
-				alias: 'I',
-				describe: 'Number of items per page in paginated commands',
-				group: 'General:'
-			})
-			.option('auto-reconnect', {
-				type: 'boolean',
-				default: this.defaults.autoReconnect,
-				alias: 'a',
-				describe: 'Whether or not the bot should automatically reconnect when disconnected',
-				group: 'General:'
-			})
-			.option('storage', {
-				type: 'string',
-				default: this.defaults.storage,
-				alias: 's',
-				describe: 'Path to storage directory',
-				group: 'General:',
-				normalize: true
-			})
-			.option('update-check', {
-				type: 'number',
-				default: this.defaults.updateCheck,
-				alias: 'U',
-				describe: 'How frequently to check for an update (in minutes, use 0 to disable)',
-				group: 'General:'
-			})
+				// General
+				.option('owner', {
+					type: 'string',
+					alias: 'o',
+					describe: 'Discord user ID of the bot owner',
+					group: 'General:'
+				})
+				.option('invite', {
+					type: 'string',
+					alias: 'i',
+					describe: 'Discord instant invite to a server to contact the owner',
+					group: 'General:'
+				})
+				.option('playing-game', {
+					type: 'string',
+					default: this.defaults.playingGame,
+					alias: 'g',
+					describe: 'Text to show in the "Playing..." status',
+					group: 'General:'
+				})
+				.option('pagination-items', {
+					type: 'number',
+					default: this.defaults.paginationItems,
+					alias: 'I',
+					describe: 'Number of items per page in paginated commands',
+					group: 'General:'
+				})
+				.option('auto-reconnect', {
+					type: 'boolean',
+					default: this.defaults.autoReconnect,
+					alias: 'a',
+					describe: 'Whether or not the bot should automatically reconnect when disconnected',
+					group: 'General:'
+				})
+				.option('storage', {
+					type: 'string',
+					default: this.defaults.storage,
+					alias: 's',
+					describe: 'Path to storage directory',
+					group: 'General:',
+					normalize: true
+				})
+				.option('update-check', {
+					type: 'number',
+					default: this.defaults.updateCheck,
+					alias: 'U',
+					describe: 'How frequently to check for an update (in minutes, use 0 to disable)',
+					group: 'General:'
+				})
 
-			// Commands
-			.option('command-prefix', {
-				type: 'string',
-				default: this.defaults.commandPrefix,
-				alias: 'P',
-				describe: 'Default command prefix (blank to use only mentions)',
-				group: 'Commands:'
-			})
-			.option('command-editable', {
-				type: 'number',
-				default: this.defaults.commandEditable,
-				alias: 'E',
-				describe: 'How long a command message is editable (in seconds, use 0 to disable)',
-				group: 'Commands:'
-			})
-			.option('non-command-edit', {
-				type: 'boolean',
-				default: this.defaults.nonCommandEdit,
-				alias: 'N',
-				describe: 'Whether or not a non-command message can be edited into a command',
-				group: 'Commands:'
-			})
+				// Commands
+				.option('command-prefix', {
+					type: 'string',
+					default: this.defaults.commandPrefix,
+					alias: 'P',
+					describe: 'Default command prefix (blank to use only mentions)',
+					group: 'Commands:'
+				})
+				.option('command-editable', {
+					type: 'number',
+					default: this.defaults.commandEditable,
+					alias: 'E',
+					describe: 'How long a command message is editable (in seconds, use 0 to disable)',
+					group: 'Commands:'
+				})
+				.option('non-command-edit', {
+					type: 'boolean',
+					default: this.defaults.nonCommandEdit,
+					alias: 'N',
+					describe: 'Whether or not a non-command message can be edited into a command',
+					group: 'Commands:'
+				})
 
-			// Logging
-			.option('log', {
-				type: 'string',
-				default: this.defaults.log,
-				alias: 'l',
-				describe: 'Path to log file',
-				group: 'Logging:',
-				normalize: true
-			})
-			.option('log-max-size', {
-				type: 'number',
-				default: this.defaults.logMaxSize,
-				defaultDescription: '5MB',
-				alias: 'S',
-				describe: 'Maximum size of single log file (in bytes)',
-				group: 'Logging:'
-			})
-			.option('log-max-files', {
-				type: 'number',
-				default: this.defaults.logMaxFiles,
-				alias: 'F',
-				describe: 'Maximum amount of log files to keep',
-				group: 'Logging:'
-			})
-			.option('log-level', {
-				type: 'string',
-				default: this.defaults.logLevel,
-				alias: 'L',
-				describe: 'Log level to output to the log file (error, warn, info, verbose, debug)',
-				group: 'Logging:'
-			})
-			.option('console-level', {
-				type: 'string',
-				default: this.defaults.consoleLevel,
-				alias: 'C',
-				describe: 'Log level to output to the console (error, warn, info, verbose, debug)',
-				group: 'Logging:'
-			})
-			.option('config', {
-				type: 'string',
-				alias: 'c',
-				describe: 'Path to JSON/YAML config file',
-				group: 'Special:',
-				normalize: true,
-				config: true,
-				configParser: configFile => {
-					const extension = require('path').extname(configFile).toLowerCase();
-					if(extension === '.json') {
-						return JSON.parse(require('fs').readFileSync(configFile));
-					} else if(extension === '.yml' || extension === '.yaml') {
-						return require('js-yaml').safeLoad(require('fs').readFileSync(configFile));
+				// Logging
+				.option('log', {
+					type: 'string',
+					default: this.defaults.log,
+					alias: 'l',
+					describe: 'Path to log file',
+					group: 'Logging:',
+					normalize: true
+				})
+				.option('log-max-size', {
+					type: 'number',
+					default: this.defaults.logMaxSize,
+					defaultDescription: '5MB',
+					alias: 'S',
+					describe: 'Maximum size of single log file (in bytes)',
+					group: 'Logging:'
+				})
+				.option('log-max-files', {
+					type: 'number',
+					default: this.defaults.logMaxFiles,
+					alias: 'F',
+					describe: 'Maximum amount of log files to keep',
+					group: 'Logging:'
+				})
+				.option('log-level', {
+					type: 'string',
+					default: this.defaults.logLevel,
+					alias: 'L',
+					describe: 'Log level to output to the log file (error, warn, info, verbose, debug)',
+					group: 'Logging:'
+				})
+				.option('console-level', {
+					type: 'string',
+					default: this.defaults.consoleLevel,
+					alias: 'C',
+					describe: 'Log level to output to the console (error, warn, info, verbose, debug)',
+					group: 'Logging:'
+				})
+				.option('config', {
+					type: 'string',
+					alias: 'c',
+					describe: 'Path to JSON/YAML config file',
+					group: 'Special:',
+					normalize: true,
+					config: true,
+					configParser: configFile => {
+						const extension = require('path').extname(configFile).toLowerCase();
+						if(extension === '.json') {
+							return JSON.parse(require('fs').readFileSync(configFile));
+						} else if(extension === '.yml' || extension === '.yaml') {
+							return require('js-yaml').safeLoad(require('fs').readFileSync(configFile));
+						}
+						throw new Error('Unknown config file type.');
 					}
-					throw new Error('Unknown config file type.');
-				}
-			})
-		.argv;
+				});
+		}
+
+		this.values = yargs.argv;
 		return yargs;
 	}
 }
