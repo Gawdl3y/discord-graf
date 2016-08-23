a8c1044a846012a51f6f04c739ebbdbfadf63b22
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-08-22 21:40:08 -0400

    Added ESDocs for setting model

--- src/data/models/setting.js
+++ src/data/models/setting.js
@@ -1,11 +1,21 @@
 'use babel';
 'use strict';
 
+/** A server-specific setting */
 export default class Setting {
+	/**
+	 * @param {?Server|string} server - The server or server ID ('global' if empty)
+	 * @param {string} key - The key of the setting
+	 * @param {*} [value] - The value of the setting
+	 */
 	constructor(server, key, value) {
 		if(!key) throw new Error('Setting key must be specified.');
+
+		/** @type {string} */
 		this.server = server ? server.id ? server.id : server : 'global';
+		/** @type {string} */
 		this.key = key;
-		this.value = value;
+		/** @type {*} */
+		this.value = value || null;
 	}
 }
