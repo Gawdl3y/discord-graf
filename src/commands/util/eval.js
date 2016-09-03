13ba5fe3c7c5e73e77d3e1117d4a9eea00b62857
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-09-02 23:23:54 -0400

    Made eval callback split its result too

--- src/commands/util/eval.js
+++ src/commands/util/eval.js
@@ -45,14 +45,12 @@ export default class EvalCommand extends Command {
 			if(val instanceof Error) {
 				message.reply(`Callback error: \`${val}\``);
 			} else {
-				const hrDiff = process.hrtime(this.hrStart);
-				const inspected = util.inspect(val, { depth: 0 });
-				message.reply(tags.stripIndents`
-					${hrDiff ? `*Callback executed after ${hrDiff[0] > 0 ? `${hrDiff[0]}s ` : ''}${hrDiff[1] / 1000000}ms.*` : '*Callback*'}
-					\`\`\`javascript
-					${inspected.length < 1925 ? inspected : val}
-					\`\`\`
-				`);
+				const result = this.makeResultMessages(val, process.hrtime(this.hrStart));
+				if(Array.isArray(result)) {
+					for(const item of result) message.reply(item);
+				} else {
+					message.reply(result);
+				}
 			}
 		};
 		/* eslint-enable no-unused-vars */
@@ -67,36 +65,46 @@ export default class EvalCommand extends Command {
 			return `Error while evaluating: \`${err}\``;
 		}
 
-		// Prepare for callback execution time
+		// Prepare for callback time and respond
 		this.hrStart = process.hrtime();
-
-		// Inspect the return value, split it, and format each part properly
-		const inspected = util.inspect(this.lastResult, { depth: 0 }).replace(nlPattern, '\n');
-		const split = inspected.split('\n');
-		const last = inspected.length - 1;
-		const prependPart = inspected[0] !== '{' && inspected[0] !== '[' && inspected[0] !== "'" ? split[0] : inspected[0];
-		const appendPart = inspected[last] !== '}' && inspected[last] !== ']' && inspected[last] !== "'" ? split[split.length - 1] : inspected[last];
-		const prepend = `\`\`\`javascript\n${prependPart}\n`;
-		const append = `\n${appendPart}\n\`\`\``;
-		const response = this.bot.util.split(tags.stripIndents`
-			${this.bot.config.values.selfbot ? `
-				*Input*
-				\`\`\`javascript
-				${args[0]}
-				\`\`\``
-			: ''}
-			*Executed in ${hrDiff[0] > 0 ? `${hrDiff[0]}s ` : ''}${hrDiff[1] / 1000000}ms.*
-			\`\`\`javascript
-			${inspected}
-			\`\`\`
-		`, 1900, '\n', prepend, append);
-
+		const response = this.makeResultMessages(this.lastResult, hrDiff, args[0]);
 		if(this.bot.config.values.selfbot) {
 			await message.edit(response[0]);
 			if(response.length > 0) return response.slice(1, response.length - 1);
 			return null;
 		} else {
 			return response;
+		}
+	}
+
+	makeResultMessages(result, hrDiff, input = null) {
+		const inspected = util.inspect(result, { depth: 0 }).replace(nlPattern, '\n');
+		const split = inspected.split('\n');
+		const last = inspected.length - 1;
+		const prependPart = inspected[0] !== '{' && inspected[0] !== '[' && inspected[0] !== "'" ? split[0] : inspected[0];
+		const appendPart = inspected[last] !== '}' && inspected[last] !== ']' && inspected[last] !== "'" ? split[split.length - 1] : inspected[last];
+		const prepend = `\`\`\`javascript\n${prependPart}\n`;
+		const append = `\n${appendPart}\n\`\`\``;
+		if(input) {
+			return this.bot.util.split(tags.stripIndents`
+				${this.bot.config.values.selfbot ? `
+					*Input*
+					\`\`\`javascript
+					${input}
+					\`\`\``
+				: ''}
+				*Executed in ${hrDiff[0] > 0 ? `${hrDiff[0]}s ` : ''}${hrDiff[1] / 1000000}ms.*
+				\`\`\`javascript
+				${inspected}
+				\`\`\`
+			`, 1900, '\n', prepend, append);
+		} else {
+			return this.bot.util.split(tags.stripIndents`
+				*Callback executed after ${hrDiff[0] > 0 ? `${hrDiff[0]}s ` : ''}${hrDiff[1] / 1000000}ms.*
+				\`\`\`javascript
+				${inspected}
+				\`\`\`
+			`, 1900, '\n', prepend, append);
 		}
 	}
 }
